b248959bdb2bba311f1d8d1264d20e2ee808c375
AKRO <habiba31245@gmail.com>
2026-08-03 19:16:20 +0300

    Add files via upload

--- app/src/main/jni/include/syscall.h
+++ app/src/main/jni/include/syscall.h
@@ -23,3 +23,26 @@ __attribute__((always_inline)) inline long __syscall_arm64(long sysno, long arg1
 
 #define SYSCALL(sysno, ...) __syscall_arm64(sysno, ##__VA_ARGS__)
 #endif
+
+#ifdef __arm__
+__attribute__((always_inline)) inline long __syscall_arm32(long sysno, long arg1 = 0, long arg2 = 0, long arg3 = 0, long arg4 = 0, long arg5 = 0, long arg6 = 0) {
+    register long r0 asm("r0") = arg1;
+    register long r1 asm("r1") = arg2;
+    register long r2 asm("r2") = arg3;
+    register long r3 asm("r3") = arg4;
+    register long r4 asm("r4") = arg5;
+    register long r5 asm("r5") = arg6;
+    register long r7 asm("r7") = sysno;
+
+    asm volatile(
+        "svc #0\n"
+        : "=r" (r0)
+        : "r" (r0), "r" (r1), "r" (r2), "r" (r3), "r" (r4), "r" (r5), "r" (r7)
+        : "memory", "cc"
+    );
+
+    return r0;
+}
+
+#define SYSCALL(sysno, ...) __syscall_arm32(sysno, ##__VA_ARGS__)
+#endif
